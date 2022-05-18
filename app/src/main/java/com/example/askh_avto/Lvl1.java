ffefc6b89c857cf353356a189683a8c07554a02b
Askhab <Abdrazakov393Gmail.com>
2022-05-18 15:35:26 +0300

    18.05.22

--- app/src/main/java/com/example/askh_avto/Lvl1.java
+++ app/src/main/java/com/example/askh_avto/Lvl1.java
@@ -2,12 +2,16 @@ package com.example.askh_avto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,8 +25,10 @@ public class Lvl1 extends AppCompatActivity {
     public int numRight; //Переменная для правой картинки + текст
     Array array = new Array(); //Создали новый объект из класса Array
     Random random = new Random(); //Для генерации случайных чисел
+    public int count = 0; //Счетчик правильных ответов
 
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -71,6 +77,17 @@ public class Lvl1 extends AppCompatActivity {
         });
         //кнопка НАЗАД - конец
 
+        //Массив для прогресса игры - начало
+        final int[] progress =
+                {
+                        R.id.point1, R.id.point2, R.id.point3, R.id.point4, R.id.point5, R.id.point6, R.id.point7, R.id.point8, R.id.point9, R.id.point10,
+                };
+        //Массив для прогресса игры - конец
+
+        //подключаем анимацию - начало
+        final Animation a = AnimationUtils.loadAnimation(Lvl1.this, R.anim.alpha);
+        //подключаем анимацию - конец
+
         numLeft = random.nextInt(10); //Генерируем случайное число от 0 до 9
         img_left.setImageResource(array.images1[numLeft]); //Достаем из массива картинку
         text_left.setText(array.texts1[numLeft]); //Достаем из массива текст
@@ -86,6 +103,59 @@ public class Lvl1 extends AppCompatActivity {
 
         img_right.setImageResource(array.images1[numRight]); //Достаем из массива картинку
         text_right.setText(array.texts1[numRight]); //Достаем из массива текст
+
+        //Обрабатываем нажатие на левую картинку - начало
+        img_left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //Условие касания картинки - начало
+                if (event.getAction()==MotionEvent.ACTION_DOWN)
+                {
+                    //Если коснулся картинки - начало
+                    img_right.setEnabled(false); //Блокируем правую картинку
+                    if (numLeft>numRight)
+                    {
+                        img_left.setImageResource(R.drawable.img_true);
+                    }
+                    else
+                    {
+                        img_left.setImageResource(R.drawable.img_false);
+                    }//Если коснулся картинки - конец
+                }
+                else if (event.getAction()==MotionEvent.ACTION_UP)
+                {
+                    //Если отпустил палец - начало
+                    if (numLeft>numRight)
+                    {//Если левая картинка больше
+                        if (count<10)
+                        {
+                            count = count + 1;
+                        }
+
+                        //Закрашиваем прогресс серым цветом - начало
+                        for (int i=0; i<10; i++)
+                        {
+                            TextView tv = findViewById(progress[i]);
+                            tv.setBackgroundResource(R.drawable.style_points);
+                        }
+                    }
+                        //Закрашиваем прогресс серым цветом - конец
+
+                    //Определяем правильные ответы и закрашиваем зеленым - начало
+                    for (int i=0; i<count; i++)
+                    {
+                        TextView tv = findViewById(progress[i]);
+                        tv.setBackgroundResource(R.drawable.style_points_green);
+                    }
+                    //Определяем правильные ответы и закрашиваем зеленым - конец
+                    }else{
+                    //Если отпустил палец - конец
+                }
+                //Условие касания картинки -конец
+                return true;
+            }
+        });
+        //Обрабатываем нажатие на левую картинку - конец
 
 
     }
